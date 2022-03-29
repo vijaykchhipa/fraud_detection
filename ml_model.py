ec51f0b5ee758f4f6bf8bb2bec9831cc5b90bcc2
vijaykchhipa <vijaych89@gmail.com>
2022-03-29 23:57:01 +0530

    create new mdoel code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,13 @@
 
 
 print("adding all ml related code here")
+
+def model1():
+    print("adding code for ML model 1")
+
+def model2():
+    print("adding code for ML model 2")
+
+
+def model3():
+    print("adding code for ML model 3")
